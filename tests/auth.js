656833e48c2732637d6e00901767afa4600d7d54
Gabriel Reiser <gabriel.reiser@mybrighthouse.com>
2013-11-22 14:20:12 -0500

    Added LDAP authentication module with app module and authentication test script
    Signed-off-by: Gabriel Reiser <gabriel.reiser@mybrighthouse.com>

---
+++ tests/auth.js
@@ -0,0 +1,18 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: GXReiser
+ * Date: 11/22/13
+ * Time: 1:43 PM
+ * To change this template use File | Settings | File Templates.
+ */
+var settings = require('./../settings.json');
+var ldauth = require('./../modules/authentication/ldap');
+
+var auth = new ldauth();
+
+auth.login("username", "password", function(err, user){
+    if(err){console.log(err);}
+    if(user){console.log(user);}
+    auth.close();
+    process.exit(0);
+});
